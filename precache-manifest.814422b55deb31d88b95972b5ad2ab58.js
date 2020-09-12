a4c9b1e54a925e44bb955e483f0ec3f9f150247b
Evans Kibet <ekibet@onfonmedia.com>
2020-09-12 17:49:47 +0300

    Updates

---
+++ precache-manifest.814422b55deb31d88b95972b5ad2ab58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24819ab96d55ac5ae724ad52a6737e25",
+    "url": "/youtube-clone-reactjs/index.html"
+  },
+  {
+    "revision": "fd91eb12cfad4613ea28",
+    "url": "/youtube-clone-reactjs/static/css/main.7ab4eccd.chunk.css"
+  },
+  {
+    "revision": "c453cabb26231e8e245a",
+    "url": "/youtube-clone-reactjs/static/js/2.52f99f1c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone-reactjs/static/js/2.52f99f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd91eb12cfad4613ea28",
+    "url": "/youtube-clone-reactjs/static/js/main.d3c0f164.chunk.js"
+  },
+  {
+    "revision": "bd05ad69e777399b46f0",
+    "url": "/youtube-clone-reactjs/static/js/runtime-main.e00a0397.js"
+  }
+]);
